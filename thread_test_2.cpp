98ae30828d2ebb1c8703a7298c15e62d6451f4af
Kartik Nighojkar <karsohn@gmail.com>
2019-02-26 22:04:36 -0800

    Changed threading

--- thread_test_2.cpp
+++ thread_test_2.cpp
@@ -34,19 +34,15 @@ VideoWriter writer("out.avi", CAP_FFMPEG, CV_FOURCC('I', 'Y', 'U', 'V'), 30, Siz
 void read() {
   vcap.set(CV_CAP_PROP_FRAME_WIDTH, frame_width);
   vcap.set(CV_CAP_PROP_FRAME_HEIGHT, frame_height);
-  // vcap.set(CV_CAP_PROP_FPS, FPS);
+  vcap.set(CV_CAP_PROP_FPS, FPS);
   Mat frame;
   vcap >> frame;
-  // imshow("image", frame);
-  // waitKey(0);
   currentFrame = frame;
   frameIndex = frameIndex + 1;
   cout << "Read frame" << '\n';
 }
 
 void write(VideoWriter write) {
-  imshow("image", currentFrame);
-  waitKey(1);
   writer.write(currentFrame);
   cout << "Wrote frame " << '\n';
   cout << frameIndex << '\n';
@@ -60,14 +56,13 @@ void update() {
 
 int main(int argc, char const *argv[]) {
   /* code */
-  int i = 0;
-  while (i < 300) {
-    update();
-    i++;
-  }
-  //new thread readThread(read);
-  //new thread writeThread(write, writer);
-  //read();
-  //write(writer);
+  //int i = 0;
+ // while (i < 300) {
+ //   update();
+  //  i++;
+ // }
+
+  new thread readThread(read);
+  new thread writeThread(write, writer);
   return 0;
 }
