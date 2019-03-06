b820f2625ced4fdafb788e6a55b8f540ca46eecf
Kartik Nighojkar <karsohn@gmail.com>
2019-03-06 12:45:25 -0800

    Made changes

--- thread_test_2.cpp
+++ thread_test_2.cpp
@@ -13,8 +13,8 @@ using namespace std;
 using namespace cv;
 using namespace chrono;
 
-#define NUM_THREADS 2
-#define FPS 30
+int NUM_THREADS = 2;
+int FPS = 30;
 
 // General constants
 Mat currentFrame;
@@ -24,45 +24,40 @@ int frame_height = 2160;
 
 // Set up reader
 VideoCapture vcap(0);
-// vcap.set(CV_CAP_PROP_FRAME_WIDTH, frame_width);
-// vcap.set(CV_CAP_PROP_FRAME_HEIGHT, frame_height);
+vcap.set(CV_CAP_PROP_FRAME_WIDTH, frame_width);
+vcap.set(CV_CAP_PROP_FRAME_HEIGHT, frame_height);
+vcap.set(CV_CAP_PROP_FPS, FPS);
 
 // Set up writer
 VideoWriter writer("out.avi", CAP_FFMPEG, CV_FOURCC('I', 'Y', 'U', 'V'), 30, Size(frame_width, frame_height), true);
 
+// Vector of images
+std::vector<int> v;
+
 
 void read() {
-  vcap.set(CV_CAP_PROP_FRAME_WIDTH, frame_width);
-  vcap.set(CV_CAP_PROP_FRAME_HEIGHT, frame_height);
-  vcap.set(CV_CAP_PROP_FPS, FPS);
   Mat frame;
   vcap >> frame;
   currentFrame = frame;
   frameIndex = frameIndex + 1;
   cout << "Read frame" << '\n';
+  return;
 }
 
-void write(VideoWriter write) {
+void write() {
   writer.write(currentFrame);
   cout << "Wrote frame " << '\n';
   cout << frameIndex << '\n';
+  return;
 }
 
-void update() {
-  read();
-  write(writer);
-  //this_thread::sleep_for(milliseconds(30));
-}
 
 int main(int argc, char const *argv[]) {
-  /* code */
-  //int i = 0;
- // while (i < 300) {
- //   update();
-  //  i++;
- // }
 
-  new thread readThread(read);
-  new thread writeThread(write, writer);
+  thread readThread(read);
+  thread writeThread(write);
+
+  readThread.join();
+  writeThread.join();
   return 0;
 }
